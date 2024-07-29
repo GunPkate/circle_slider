41d271a3f2d014ea3f1e1a92ccd12a8adcfbb706
DESKTOP-GFBT1N7\GunP <gundash1@gmail.com>
2024-07-29 22:52:02 +0700

    gsap animate v1

--- mainScreen.js
+++ mainScreen.js
@@ -18,3 +18,42 @@ document.addEventListener("DOMContentLoaded",function(){
         })
     }
 })
+
+function LottieScrollTrigger(vars){
+    let playhead = { frame: 0},
+    target = gsap.utils.toArray(vars.target)[0],
+    speeds = { slow: "+2000" , medium: "+1000", fast: "+500" }
+    st = {
+        trigger: ".trigger",
+        end: speeds[vars.speeds] || "+1000",
+        scrub: 1,
+        markers: false
+    }
+    ctx = gsap.context && gsap.context(),
+    animation = lottie.loadAnimation({
+        container: target,
+        renderer: vars.renderer || "svg",
+        loop: false,
+        autoplay: false,
+        path: vars.path,
+        rendererSettings: vars.rendererSettings || {
+            preserveAspectRatio: "xMidyMid slice"
+        }
+    })
+    for (let i in vars) {
+        st[i] = vars[i]  
+    }
+
+    animation.addEventListener("DOMLoaded", function (){
+        let createTween = function() {
+            animation.frameTween = gsap.to(playhead,{
+                frame: animation.totalFrames - 1,
+                ease: "none",
+                onUpdate: () => animation.goToAndStop(playhead.frame,true),
+                scrollTrigger: st
+            })
+            return () => animation.destroy && animation.destroy(); 
+        }
+        ctx && ctx.add ? add(createTween) : createTween()
+    })
+}
